f0436cd7cf89bb95082088565c5c9e00120afcc4
TheVineCode <keaganforwork@gmail.com>
2026-06-01 18:55:03 +0200

    Setting up first routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
@@ -16,11 +17,19 @@ app.use(cookieParser());
 // write: callback function
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
 
-
-
 app.get('/', (req, res) => {
   logger.info('Hello from Acquistions!');
   res.status(200).send('Hello from Acquistions!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquistion API is running'});
+})
+
+app.use('/api/auth', authRoutes); //api/auth
 
 export default app;
